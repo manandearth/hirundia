3e63099a9a207952d265be0116a8b5b1972ae3ef
manandearth <adamgefen@posteo.net>
2019-03-31 23:50:58 +0200

    More to register endpoint

---
+++ src/hirundia/services/session/register/logic.clj
@@ -0,0 +1,14 @@
+(ns hirundia.services.session.register.logic
+  (:require
+   [honeysql.helpers :as hh]
+   [honeysql.core :as h]))
+
+(defn to-insert [username password]
+  (-> (hh/insert-into :register)
+      (hh/values [{:username username
+                   :password password}])))
+
+(defn to-check [username]
+  (-> (hh/select :username)
+      (hh/from :register)
+      (hh/where [:= :username username])))

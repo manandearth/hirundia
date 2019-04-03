1318de2dac4ec753e938b8a9ea0702b52a11487d
manandearth <adamgefen@posteo.net>
2019-04-03 22:03:32 +0200

    Introduce authorization for delete (admin only)

--- src/hirundia/services/nests/delete/endpoint.clj
+++ src/hirundia/services/nests/delete/endpoint.clj
@@ -3,24 +3,25 @@
    [clojure.spec.alpha :as spec]
    [clojure.java.jdbc :as jdbc]
    [ring.util.response :as ring-resp]
-   [buddy.auth :refer [authenticated?]]
+   [buddy.auth :refer [authenticated? throw-unauthorized]]
    [honeysql.core :as h]
    [hirundia.services.nests.delete.logic :as logic]
-   [hirundia.services.nests.retrieveall.logic :as retrieveall.logic]))
+   [hirundia.services.nests.retrieveall.logic :as retrieveall.logic]
+   [hirundia.services.session.login.endpoint :as session.login]))
 
 (spec/def ::id nat-int?)
 
 (spec/def ::api (spec/keys :req-un [::id]))
 
 (defn perform [{{:keys [id]} :path-params :keys [db session] :as request}]
-  (if (authenticated? session)
-    (let [db (->> db :pool (hash-map :datasource))
-          _  (->> (logic/to-delete id)
-                  (h/format)
-                  (jdbc/execute! db))]
-      (-> (ring-resp/redirect "/nests")
-          (assoc :flash (str "Entry " id " has beed deleted.")))
-      #_{:status 302 :headers {"Location" "/nests"} :body "" :flash (str "Entry " id " has beed deleted.")})
-    (-> (ring-resp/redirect "/nests")
-        (assoc :flash "login in order to delete an entry")))
-  )
+  (let [known-user (session.login/username-password-role request (:identity session))]
+    (if (= :admin (keyword (:role known-user)))
+      (let [db (->> db :pool (hash-map :datasource))
+            _  (->> (logic/to-delete id)
+                    (h/format)
+                    (jdbc/execute! db))]
+        (-> (ring-resp/redirect "/nests")
+            (assoc :flash (str "Entry " id " has beed deleted."))))
+      (throw-unauthorized))))
+
+

42b0569a9679b5a3598f356ae2c884f6ce1d280c
manandearth <adamgefen@posteo.net>
2019-05-09 19:56:37 +0200

    Fix `nests-delete/:id` route

--- src/clj/hirundia/services/nests/delete/endpoint.clj
+++ src/clj/hirundia/services/nests/delete/endpoint.clj
@@ -3,23 +3,21 @@
    [clojure.spec.alpha :as spec]
    [clojure.java.jdbc :as jdbc]
    [ring.util.response :as ring-resp]
-   [buddy.auth :refer [authenticated? throw-unauthorized]]
+   [io.pedestal.http.route :refer [url-for]]
    [honeysql.core :as h]
    [hirundia.services.nests.delete.logic :as logic]
    [hirundia.services.nests.retrieveall.logic :as retrieveall.logic]
-   [hirundia.services.session.login.endpoint :as session.login]))
+   [hirundia.services.session.login.endpoint :as session.login]
+   [clojure.java.io :as io]))
 
 (spec/def ::id nat-int?)
 
 (spec/def ::api (spec/keys :req-un [::id]))
 
 (defn perform [{{:keys [id]} :path-params :keys [db session] :as request}]
-  (let [known-user (session.login/username-password-role request (:identity session))]
-    (if (= :admin (keyword (:role known-user)))
-      (let [db (->> db :pool (hash-map :datasource))
-            _  (->> (logic/to-delete id)
-                    (h/format)
-                    (jdbc/execute! db))]
-        (-> (ring-resp/redirect :nests)
-            (assoc :flash (str "Entry " id " has beed deleted."))))
-      (throw-unauthorized))))
+  (let [db (->> db :pool (hash-map :datasource))
+        _  (->> (logic/to-delete id)
+                (h/format)
+                (jdbc/execute! db))]
+    (-> (ring-resp/redirect (url-for :nests))
+        (assoc :flash (str "Entry " id " has beed deleted.")))))

b52b6353d1653546b274d252dcffe2862c175e38
manandearth <adamgefen@posteo.net>
2019-05-26 08:09:04 +0200

    Fix tidy up for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/hirundia.jar clojure.main -m hirundia.server
--- src/clj/hirundia/pages.clj
+++ src/clj/hirundia/pages.clj
@@ -4,12 +4,11 @@
             [hiccup.table :as table]))
 
 (def sample-entry (vector (zipmap [:id :street :number :gps :species :height :facing :type :date :destroyed :destroyed_date :delete "delete 23"] [23 "Juan Bueno" 1 "(32.677 0.201)" "swallow" 5 "N" "crack" "2019-04-04" false nil])))
-sample-entry
+
 (defn intro []
   [:div
    [:h2 "From Wikipedia " [:a {:href "https://en.wikipedia.org/wiki/Swallow"} "Swallow:"]]
-   [:p "The swallows, martins and saw-wings, or Hirundinidae, are a family of passerine birds found around the world on all continents, including occasionally in Antarctica. Highly adapted to aerial feeding, they have a distinctive appearance. The term Swallow is used colloquially in Europe as a synonym for the barn swallow. There are around 90 species of Hirundinidae, divided into 19 genera, with the greatest diversity found in Africa, which is also thought to be where they evolved as hole-nesters. They also occur on a number of oceanic islands. A number of European and North American species are long-distance migrants by contrast, the West and South African swallows are non-migratory."
-    ]
+   [:p "The swallows, martins and saw-wings, or Hirundinidae, are a family of passerine birds found around the world on all continents, including occasionally in Antarctica. Highly adapted to aerial feeding, they have a distinctive appearance. The term Swallow is used colloquially in Europe as a synonym for the barn swallow. There are around 90 species of Hirundinidae, divided into 19 genera, with the greatest diversity found in Africa, which is also thought to be where they evolved as hole-nesters. They also occur on a number of oceanic islands. A number of European and North American species are long-distance migrants by contrast, the West and South African swallows are non-migratory."]
    [:h2 "The project"]
    [:p "Hirundia is a data collection tool for investigating the effects of humans on the populations of migrating birds. Users have to register an account in order to update the database. "]
    [:p "Each nest makes an entry, like a row in an excel sheet"]
@@ -30,28 +29,26 @@ sample-entry
       :date "Date"
       :destroyed "Destroyed?"
       :destroyed_date "Date destroyed"
-      :delete "Delete Entry"]
-     )
-   #_ (table/to-table1d
-     {:id 1 :name "boy"}
-     [:id "ID"
-      :name "NAME"])]
+      :delete "Delete Entry"])
+    #_(table/to-table1d
+       {:id 1 :name "boy"}
+       [:id "ID"
+        :name "NAME"])]
    [:div
     [:h2 "example form:"]
     [:form {:action "/" :method "GET"}
        ;;(util/anti-forgery-field) ; prevents cross-site scripting attacks
-       [:div
-        [:p [:label.justify "Street: " [:input {:type "text" :name "street"}]]]
-        [:p [:label.justify "Number: " [:input {:type "int" :name "number"}]]]
-        [:p [:label.justify "Latitude: " [:input {:type "int" :name "lat" }]]]
-        [:p [:label.justify "Longitude: " [:input {:type "int" :name "lon" }]]]
-        [:p [:label.justify "Species: " (form/drop-down "species" ["swallow" "swift" "martin"])]]
-        [:p [:label.justify "Height: " (form/drop-down "height" (map inc (range 20)))]]
-        [:p [:label.justify "Facing: "(form/drop-down "facing" ["N" "NW" "W" "SW" "S" "SE" "E" "NE"])]]
-        [:p [:label.justify "Type: " (form/drop-down "type-of" ["balcony" "window" "cornice" "gable" "cables" "crack"])]]
-        [:p [:label.justify "Date: " [:input {:type "date" :name "date"}]]]
-        [:p "If the nest is no longer there fill in the following and include the day recorded:"]
-        [:p [:label.justify "Destroyed: " (form/drop-down "destroyed" ["true" "false"])]]
-        [:p [:label.justify "Destroyed date: " [:input {:type "date" :name "destroyed_date"}]]]
-        [:p [:label.justify "λ ->" [:input {:type "submit" :value "Update"}]]]]
-       ]]])
+     [:div
+      [:p [:label.justify "Street: " [:input {:type "text" :name "street"}]]]
+      [:p [:label.justify "Number: " [:input {:type "int" :name "number"}]]]
+      [:p [:label.justify "Latitude: " [:input {:type "int" :name "lat"}]]]
+      [:p [:label.justify "Longitude: " [:input {:type "int" :name "lon"}]]]
+      [:p [:label.justify "Species: " (form/drop-down "species" ["swallow" "swift" "martin"])]]
+      [:p [:label.justify "Height: " (form/drop-down "height" (map inc (range 20)))]]
+      [:p [:label.justify "Facing: " (form/drop-down "facing" ["N" "NW" "W" "SW" "S" "SE" "E" "NE"])]]
+      [:p [:label.justify "Type: " (form/drop-down "type-of" ["balcony" "window" "cornice" "gable" "cables" "crack"])]]
+      [:p [:label.justify "Date: " [:input {:type "date" :name "date"}]]]
+      [:p "If the nest is no longer there fill in the following and include the day recorded:"]
+      [:p [:label.justify "Destroyed: " (form/drop-down "destroyed" ["true" "false"])]]
+      [:p [:label.justify "Destroyed date: " [:input {:type "date" :name "destroyed_date"}]]]
+      [:p [:label.justify "λ ->" [:input {:type "submit" :value "Update"}]]]]]]])

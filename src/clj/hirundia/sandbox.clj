52e641915116e29f5d54e7abbea35114058769a2
manandearth <adamgefen@posteo.net>
2019-05-27 19:02:01 +0200

    Clean sandbox [2]

--- src/clj/hirundia/sandbox.clj
+++ src/clj/hirundia/sandbox.clj
@@ -149,9 +149,9 @@
      db
      test-map))
 
-(def select-all-query
-  (jdbc/query conn (sql/format  {:select [:*]
-                                 :from [:nests]})))
+;; (def select-all-query
+;;   (jdbc/query conn (sql/format  {:select [:*]
+;;                                  :from [:nests]})))
 
 (defn insert-entry [address]
   (let [results

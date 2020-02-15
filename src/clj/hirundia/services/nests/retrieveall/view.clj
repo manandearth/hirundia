09be0eba9078581b83f4a73701eb19ac654e4c23
manandearth <adamgefen@posteo.net>
2020-02-15 17:45:33 +0100

    Update the nests list to spanish

--- src/clj/hirundia/services/nests/retrieveall/view.clj
+++ src/clj/hirundia/services/nests/retrieveall/view.clj
@@ -2,7 +2,15 @@
   (:require [hiccup.page :as page]
             [hiccup.table :as table]
             [hirundia.views :as views]
+            [hirundia.translate :as t]
             [io.pedestal.http.route :refer [url-for]]))
+
+(defn records-to-spanish
+  [records]
+  (map #(assoc % :destroyed (t/to-spanish (keyword (str (:destroyed %)))))
+       (map #(assoc % :facing (t/to-spanish (keyword (:facing %))))
+            (map #(assoc % :type (t/to-spanish (keyword (:type %))))
+                 (map #(assoc % :species (t/to-spanish (keyword (:species %)))) records)))))
 
 (defn all-nests [{:keys [flash] :as request} records]
   (page/html5
@@ -12,28 +20,28 @@
     (when (seq flash) [:div {:class "alert alert-warning alert-dismissible fade show" :role "alert"} flash
                        [:button {:type "button" :class "close" :data-dismiss "alert" :aria-label "Close"}
                         [:span {:aria-hidden "true"} "&times;"]]])
-    [:h1 "Listado de entradas"]
+    [:h1 "Entradas"]
     [:div (let [attr-fns {:data-value-transform (fn [label-key v]
                                                   (if (= :id label-key)
                                                     [:a {:href (url-for :nests/:id :path-params {:id v})} v]
 
                                                     (if (= :delete label-key)
-                                                      [:a {:href (url-for :nests-delete/:id :path-params {:id v})} (str "delete " v)]
+                                                      [:a {:href (url-for :nests-delete/:id :path-params {:id v})} (str (t/to-spanish :delete) v)]
                                                       v)))}
-                extended-records (map #(assoc % :delete (:id %)) records)]
+                extended-records (map #(assoc % :delete (:id %)) (records-to-spanish records))]
             (table/to-table1d
              extended-records
              [:id "ID"
-              :street "Street"
-              :house_number_name "No./Name"
+              :street (t/to-spanish :street)
+              :house_number_name (t/to-spanish :house_number_name)
               :gps "GPS"
-              :species "Species"
-              :facing "Facing"
-              :height "Height"
-              :type "Type"
-              :date "Date"
-              :destroyed "Destroyed?"
-              :destroyed_date "Date destroyed"
-              :delete "Delete Entry"
-              :author "Username"]
+              :species (t/to-spanish :species)
+              :facing (t/to-spanish :facing)
+              :height (t/to-spanish :height)
+              :type (t/to-spanish :type)
+              :date (t/to-spanish :date)
+              :destroyed (t/to-spanish :destroyed)
+              :destroyed_date (t/to-spanish :destroyed_date)
+              :delete (t/to-spanish :delete_entry)
+              :author (t/to-spanish :username)]
              attr-fns))]]))

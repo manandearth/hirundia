835c4dc894e5c08cf7589b7171ab0fa0c7230815
manandearth <adamgefen@posteo.net>
2020-02-22 22:14:21 +0100

    Update the disabled form in `about` page

--- src/clj/hirundia/pages.clj
+++ src/clj/hirundia/pages.clj
@@ -1,7 +1,8 @@
 (ns hirundia.pages
   (:require [hiccup.core :as h]
             [hiccup.form :as form]
-            [hiccup.table :as table]))
+            [hiccup.table :as table]
+            [hirundia.translate :as t]))
 
 (def sample-entry (vector (zipmap [:id :street :house_number_name :gps :species :height :facing :type :date :destroyed :destroyed_date :delete "borrar 23"] [23 "Juan Bueno" 1 "(32.677 0.201)" "Golondrina Común" 5 "norte" "grieta" "2019-04-04" "No" nil])))
 
@@ -38,22 +39,44 @@
     [:h2 "Formulario de ejemplo:"]
     [:h2 "Tenga en cuenta que este formulario está desactivado y que las entradas deben ser enviadas una vez que se haya iniciado la sesión a través del enlace 'Añadir un nido' de arriba.
 "]
-    [:form {:action "/" :method "GET"}
-       ;;(util/anti-forgery-field) ; prevents cross-site scripting attacks
-     [:div
-      [:p [:label.justify "Calle: " [:input {:type "text" :name "street"}]]]
-      [:p [:label.justify "No./nombre: " [:input {:type "text" :name "house_number_name"}]]]
-      [:p [:label.justify "Latitud: " [:input {:type "int" :name "lat"}]]]
-      [:p [:label.justify "Longitud: " [:input {:type "int" :name "lon"}]]]
-      [:p [:label.justify "Especie: " (form/drop-down "species" ["golondrina común" "vencejo común" "avión común"])]]
-      [:p [:label.justify "Altura: " (form/drop-down "height" (map inc (range 20)))]]
-      [:p [:label.justify "Orientación: " (form/drop-down "facing" ["norte" "noretoeste" "oeste" "sudoeste" "sur" "sudeste" "este" "norteste"])]]
-      [:p [:label.justify "Construcción: " (form/drop-down "type-of" ["balcón" "ventana" "cornisa" "tejado" "cable" "grieta"])]]
-      [:p [:label.justify "Fecha: " [:input {:type "date" :name "date"}]]]
-      [:p  "Cada nido crea una entrada en la base de datos. En el caso de múltiples nidos con las mismas especificaciones se actualiza este valor:
-"]
-      [:p [:label.justify "Cantidad: "       [:input {:type "int" :name "qty" :value 1}]]]
-      [:p "Si el nido ya no está allí, rellene lo siguiente e incluya el día registrado:"]
-      [:p [:label.justify "Destruida: " (form/drop-down "destroyed" ["si" "no"])]]
-      [:p [:label.justify "Fecha de destrucción: " [:input {:type "date" :name "destroyed_date"}]]]
-      [:p [:label.justify "λ ->" [:input {:type "submit" :value "Actualiza"}]]]]]]])
+    [:div {:class "container"}
+     [:h1 "Añada nido al base de datos"]
+     [:div {:class "card"}
+      [:form {:action "/nests-insert" :method "POST"}
+      ;;(util/anti-forgery-field) ; prevents cross-site scripting attacks
+       [:fieldset {:disabled "true"}
+        ;;TODO language is hard coded for now
+
+        [:p [:label [:input {:type "hidden" :name "language" :value "spanish"}]]]
+
+        [:div {:class "form-group row"}
+         [:div {:class "col-auto"} [:label {:class "my-1 mr-2"} (t/to-spanish :street)]
+          [:input {:class "form-control mx-sm-1" :id "disabledInput" :type "text" :name "street"}]]
+         [:div {:class "col-auto"} [:label {:class "my-1 mr-2"} (t/to-spanish :house_number_name)]
+          [:input {:class "form-control mx-sm-1" :type "text" :name "house_number_name"}]]]
+
+        [:div {:class "form-group row"}
+         [:div {:class "col-auto"} [:label {:class "my-1 mr-2"} (t/to-spanish :lat)]
+          [:input {:class "form-control mx-sm-1" :type "int" :name "lat"}]]
+         [:div {:class "col-auto"} [:label {:class "my-1 mr-2"} (t/to-spanish :lon)]
+          [:input {:class "form-control mx-sm-1" :type "int" :name "lon"}]]]
+
+        [:div {:class "form-group row"}
+         [:div {:class "col-auto"} [:label (t/to-spanish :species)   (form/drop-down "species" (map #(t/to-spanish %) [:swallow :swift :martin :pallid_swift :red_rumped_swallow]))]]
+         [:div {:class "col-auto"} [:label (t/to-spanish :type)      (form/drop-down "type-of" [(t/to-spanish :window) (t/to-spanish :cornice) (t/to-spanish :crack) (t/to-spanish :cables) (t/to-spanish :gable) (t/to-spanish :balcony)] (t/to-spanish :window))]]]
+
+        [:div {:class "form-group row"}
+         [:div {:class "col-auto"} [:label (t/to-spanish :height)    (form/drop-down "height" (map inc (range 20)) 5)]]
+         [:div {:class "col-auto"} [:label (t/to-spanish :facing)   (form/drop-down "facing" [(t/to-spanish :N) (t/to-spanish :NE) (t/to-spanish :E) (t/to-spanish :SE) (t/to-spanish :S) (t/to-spanish :SW) (t/to-spanish :W) (t/to-spanish :NW)] (t/to-spanish :N))]]]
+
+        [:div [:label (t/to-spanish :date)      [:input {:type "date" :name "date"}]]]
+        [:div {:class "card"} [:div {:class "card-body" :style "background-color: #f5faff"}
+                               [:div {:class "card-subtitle mb-2 text-muted"} "Cada nido crea una entrada en la base de datos. "]
+                               [:div {:class "card-subtitle mb-2 text-muted"} "En el caso de múltiples nidos con las mismas especificaciones se actualiza este valor:"]
+                               [:div (:class "col-auto") [:label (t/to-spanish :qty)       [:input {:class "form-control ms-mx-1" :type "int" :name "qty" :value 1}]]]]]
+        [:div {:class "card"} [:div {:class "card-body" :style "background-color: #e3f2fd;"}
+                               [:div {:class "card-subtitle mb-2 text-muted"} "Si el nido ya no está allí, rellene lo siguiente e incluya el día registrado:"]
+                               [:div [:label (t/to-spanish :destroyed) (form/drop-down "destroyed" [(t/to-spanish :false) (t/to-spanish :true)] (t/to-spanish :false))]]
+                               [:div [:label (t/to-spanish :destroyed_date) [:input {:type "date" :name "destroyed_date"}]]]]]
+        [:div {:class "col-auto"} [:input {:type "submit" :class "btn btn-primary mb-2" :value (t/to-spanish :submit)}]]]]]]]])
+

2f536724c038681b23b02d69843f305278f5cacd
manandearth <adamgefen@posteo.net>
2020-03-06 22:01:57 +0100

    Add responsive styles

--- src/clj/hirundia/views.clj
+++ src/clj/hirundia/views.clj
@@ -10,6 +10,12 @@
    [hirundia.pages :as pages]
    [hirundia.translate :as t]))
 
+(defn  bootstrap-scripts
+  []
+  [:script {:src "https://code.jquery.com/jquery-3.4.1.slim.min.js" :integrity "sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" :crossorigin "anonymous"}]
+  [:script {:src "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" :integrity "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" :crossorigin "anonymous"}]
+  [:script {:src "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" :integrity "sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" :crossorigin "anonymous"}])
+
 (defn gen-page-head
   [title]
   [:head
@@ -17,33 +23,43 @@
    [:link  {:rel "stylesheet" :href "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" :integrity "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" :crossorigin "anonymous"}]
    [:link {:href "https://fonts.googleapis.com/css?family=Roboto" :rel "stylesheet"}]
    [:meta {:charset "UTF-8"}]
-   (page/include-css "/css/styles.css")])
-
-(defn  bootstrap-scripts
-  []
-  [:script {:src "https://code.jquery.com/jquery-3.4.1.slim.min.js" :integrity "sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" :crossorigin "anonymous"}]
-  [:script {:src "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" :integrity "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" :crossorigin "anonymous"}]
-  [:script {:src "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" :integrity "sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" :crossorigin "anonymous"}])
+   [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+   [:script {:src "https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"}]
+   (page/include-css "/css/styles.css")
+   (bootstrap-scripts)])
 
 (defn header-links
   [{:keys [session] :as request}]
   (if  (:identity session)
     [:div {:class "container"}
-     [:ul {:class "nav fixed-top justify-content-center nav-pills" :style "background-color: #e3f2fd;"}
-      [:li {:class "nav-item"} [:a {:class "nav-link" :href "/"} [:h2 [:span {:class "badge badge-secondary"} "Hirundia"]]]]
-      [:li {:class "nav-item"} [:a {:href "/nests" :class "nav-link text-capitalize"} (t/to-spanish :nests)]]
-      [:li {:class "nav-item"} [:a {:href "/dashboard" :class "nav-link text-capitalize"} (t/to-spanish :dashboard)]]
-      [:li {:class "nav-item"} [:a {:href "/nests-insert" :class "nav-link text-capitalize"} (t/to-spanish :nests-insert)]]
-      [:li {:class "nav-item"} [:a {:class "text-capitalize"} (t/to-spanish :logged_as)  [:span {:class "badge badge-secondary text-capitalize"} (get-in session [:identity :username])]]]
-      [:li {:class "nav-item"} [:a {:href "/logout" :class "nav-link text-capitalize"} (t/to-spanish :logout)]]]]
-    [:div {:class "container"}
-     [:ul {:class "nav fixed-top justify-content-center" :style "background-color: #e3f2fd;"}
+     [:nav {:class "navbar navbar-expand-lg navbar-light bg-light"}
+      [:button {:class "navbar-toggler" :type "button" :data-toggle "collapse" :data-target "#navbarTogglerDemo03" :aria-controls "navbarTogglerDemo03" :aria-expanded "false" :aria-label "Toggle navigation"}
+       [:span {:class "navbar-toggler-icon"}]]
+      [:a {:class "navbar-brand" :href "/"} [:h2 [:span {:class "badge badge-secondary"} "Hirundia"]]]
 
-      [:li {:class "nav-item"} [:a {:href "/" :class "nav-link"} [:h2 [:span {:class "badge badge-secondary"} "Hirundia"]]]]
-      [:li {:class "nav-item"} [:a {:href "/nests" :class "nav-link text-capitalize"} (t/to-spanish :nests)]]
-      [:li {:class "nav-item"} [:a {:href "/dashboard" :class "nav-link text-capitalize"} (t/to-spanish :dashboard)]]
-      [:li {:class "nav-item"} [:a {:href "/login" :class "nav-link text-capitalize"} (t/to-spanish :login)]]
-      [:li {:class "nav-item"} [:a {:href "/register" :class "nav-link text-capitalize"} (t/to-spanish :register)]]]]))
+      [:div {:class "collapse navbar-collapse" :id "navbarTogglerDemo03"}
+       [:ul {:class "navbar-nav mr-auto mt-2 mt-lg-0" :style "background-color: #e3f2fd;"}
+        [:li {:class "nav-item"} [:a {:href "/nests" :class "nav-link text-capitalize"} (t/to-spanish :nests)]]
+        [:li {:class "nav-item"} [:a {:href "/dashboard" :class "nav-link text-capitalize"} (t/to-spanish :dashboard)]]
+        [:li {:class "nav-item"} [:a {:href "/nests-insert" :class "nav-link text-capitalize"} (t/to-spanish :nests-insert)]]
+        [:li {:class "nav-item"} [:a {:class "text-capitalize"} (t/to-spanish :logged_as)  [:span {:class "badge badge-secondary text-capitalize"} (get-in session [:identity :username])]]]
+        [:li {:class "nav-item"} [:a {:href "/logout" :class "nav-link text-capitalize"} (t/to-spanish :logout)]]]]]]
+
+    [:div {:class "container"}
+
+     [:nav {:class "navbar navbar-expand-lg navbar-light bg-light"}
+      [:button {:class "navbar-toggler" :type "button" :data-toggle "collapse" :data-target "#navbarTogglerDemo03" :aria-controls "navbarTogglerDemo03" :aria-expanded "false" :aria-label "Toggle navigation"}
+       [:span {:class "navbar-toggler-icon"}]]
+      [:a {:class "navbar-brand" :href "/"} [:h2 [:span {:class "badge badge-secondary"} "Hirundia"]]]
+
+      [:div {:class "collapse navbar-collapse" :id "navbarTogglerDemo03"}
+
+       [:ul {:class "navbar-nav mr-auto mt-2 mt-lg-0" :style "background-color: #e3f2fd;"}
+
+        [:li {:class "nav-item"} [:a {:href "/nests" :class "nav-link text-capitalize"} (t/to-spanish :nests)]]
+        [:li {:class "nav-item"} [:a {:href "/dashboard" :class "nav-link text-capitalize"} (t/to-spanish :dashboard)]]
+        [:li {:class "nav-item"} [:a {:href "/login" :class "nav-link text-capitalize"} (t/to-spanish :login)]]
+        [:li {:class "nav-item"} [:a {:href "/register" :class "nav-link text-capitalize"} (t/to-spanish :register)]]]]]]))
 
 (defn home [request]
   (page/html5
